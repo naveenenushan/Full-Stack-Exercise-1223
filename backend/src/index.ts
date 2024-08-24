edc9080efa8801d7a7adbcb34d18dea558ed5a32
Naveen Enushan <naveenenushan@gmail.com>
2024-08-24 21:48:25 +0400

    feat: add graceful shutdown for server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,39 @@
 import app from './app';
 import { config } from './config';
+let shuttingDown = false;
 
 const port = config.port;
 const env = config.env;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Env: ${env}`);
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
 });
+
+function gracefulShutdown() {
+  if (shuttingDown) {
+    console.info('Already shutting down');
+    return;
+  }
+
+  shuttingDown = true;
+  console.info('Shutting down...');
+
+  server.close(() => {
+    console.info('Server closed');
+    process.exit(0);
+  });
+
+  // Force close connections after 10 seconds
+  setTimeout(() => {
+    console.error(
+      'Could not close connections in time, forcefully shutting down',
+    );
+    process.exit(1);
+  }, 10000);
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
