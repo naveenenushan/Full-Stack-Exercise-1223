79bb09e755209de89aad66f870217d7e3d9218c7
Naveen Enushan <naveenenushan@gmail.com>
2024-08-25 10:47:33 +0400

    chore: clean up unused imports

--- backend/src/controllers/healthController.ts
+++ backend/src/controllers/healthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { errorResponse, successResponse } from '../utilities/response';
+import { successResponse } from '../utilities/response';
 export const checkHealth = async (req: Request, res: Response) => {
   return successResponse(res, 'server running', 'ok');
 };
